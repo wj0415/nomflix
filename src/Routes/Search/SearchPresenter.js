c79b3f46e639d9800c943c82c03e24d0ec1517c4
WhoJung Kim <wj0415@korea.ac.kr>
2019-05-27 03:26:04 +0900

    #6.10 React Helmet

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Helmet from "react-helmet";
 import Loader from "Components/Loader";
 import Section from "Components/Section";
 import Poster from "Components/Poster";
@@ -28,6 +29,10 @@ const SearchPresenter = ({
   updateTerm
 }) => (
   <Container>
+    <Helmet>
+      <title>Search | Nomflix </title>
+    </Helmet>
+
     <Form onSubmit={handleSubmit}>
       <Input
         placeholder="Search Movies or TV Shows..."
